8c8d304545b7cf25a95b75b39d88cc4ede84cb92
SagaSpace <62926856+SagaSpace@users.noreply.github.com>
2020-04-06 18:57:06 -0700

    first commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book (models.Model):
+    title = models.CharField(max_length = 255)
+    genre = models.CharField(max_length = 255)
+    price = models.FloatField()
+
+
+class Customer (models.Model):
+    name = models.CharField(max_length = 255)
+    number = models.IntegerField()
+    address = models.CharField(max_length = 255)
